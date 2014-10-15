5b4f1648e0b8214648dcc4e052b1985e6d0385ec
Jonas Hess <jonas.hess@revier.de>
2014-10-16 01:41:54 +0200

    +-sourcebeauty

--- qr/r.php
+++ qr/r.php
@@ -1,7 +1,7 @@
 <?php
 
 /* QR-REDIRECTOR for VoKuHiLa
- * >> Author: Jonas Hess (revier online GmbH & Co.KG) [jonas.hess@revier.de] 
+ * >> Author: Jonas Hess (revier online GmbH & Co.KG) [jonas.hess@revier.de]
  * >> Function: Use Google Analytics to count the visitor and redirect him to the desired URL
  */
 
@@ -9,21 +9,21 @@
 	$kunde     = addslashes(urldecode($_GET['kunde']));
 	$projekt   = addslashes(urldecode($_GET['projekt']));
 	$urlsource = 'targets_' . $kunde .'_' . $projekt. '.txt';
-	if(!file_exists($urlsource)) die('URLs zu Kunde / Projekt nicht bekannt.');	
+	if(!file_exists($urlsource)) die('URLs zu Kunde / Projekt nicht bekannt.');
 
 	$lines = file ($urlsource);
-	
+
 	//select desired line from target urls.txt
 	$targetURL = $lines[$_GET['nr']*1-1];
-    $targetURL = preg_replace("/(\r\n|\n|\r)/", "", $targetURL);
-	
+  $targetURL = preg_replace("/(\r\n|\n|\r)/", "", $targetURL);
+
 	//find comment in URL
 	$start = strrpos($targetURL, '{');
 	$end   = strrpos($targetURL, '}');
-	
+
 	//determine event label
 	$eventlabel = 'Kunde:' . $kunde . ' | Projekt:' . $projekt . ' | CodeNr:' . $_GET['nr'];
-	
+
 	//add comment on event cat if given
 	if ($start== 0 && $end!== false)
 	{
@@ -53,22 +53,22 @@
 
 			ga('send', {
 				'hitType': 'event',
-				'eventlabelegory': 'QR-Code Aufruf',				
+				'eventlabelegory': 'QR-Code Aufruf',
 				'eventAction': '<?php echo  $targetURL ?>',
-				'eventLabel': '<?php echo $eventlabel ?>',				
+				'eventLabel': '<?php echo $eventlabel ?>',
 				'eventValue': 1,
 				'hitCallback': function() {
 					window.location.href='<?php echo  $targetURL ?>';
-				} 
-			});  
-<?php } else { ?>	
+				}
+			});
+<?php } else { ?>
 			alert('Unbekannter QR-Code');
-<?php } ?>	
+<?php } ?>
 		</script>
 		<meta name="robots" content="noindex" />
 	</head>
 	<body>
-	Einen kleinen Augenblick, es geht gleich weiter zu 
+	Einen kleinen Augenblick, es geht gleich weiter zu
 <?php
   echo '<a href="' . $targetURL . '">' . $targetURL . '</a>'
 ?>
